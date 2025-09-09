17989d8cbd3bfe67b117e7c33eeb21980eccd023
Victoriia Kyselova <134308356+VictoriaKiselova@users.noreply.github.com>
2025-09-09 18:14:33 +0300

    Changed sorting on the veterinarians page

--- src/redux/appSlice/selectors.js
+++
@@ -1,3 +0,0 @@
-export const selectorIsOpenMenu = state => state.app.isOpenMenu;
-export const selectorIsShowNotification = state => state.app.isShowNotification;
-export const selectorIsOpenForm = state => state.app.isOpenForm;
