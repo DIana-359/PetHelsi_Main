2d2c528cb7fa8e78550b63428835bdf79bcb0250
Anna Velychko <annavelychko.ya@gmail.com>
2025-09-09 20:24:24 +0200

    add ReviewIcon & EmptyCalendar

--- src/components/DoctorPage/AboutTab.tsx
+++ src/components/DoctorPage/AboutTab.tsx
@@ -1,47 +1,46 @@
-import { Veterinarian } from '@/utils/types/veterinarian';
-import Icon from '../Icon';
-import { useState } from 'react';
-import DocReviews from './DocReviews';
+import { Veterinarian } from "@/utils/types/veterinarian";
+import Icon from "../Icon";
+import { useState } from "react";
+import DocReviews from "./DocReviews";
 
 type Props = {
   veterinarian: Veterinarian;
 };
 
 const petTypeIcons: Record<string, string> = {
-  Собака: 'icon-dog',
-  Кіт: 'icon-cat',
-  Птах: 'icon-bird',
-  Гризун: 'icon-rabbit',
-  Плазун: 'icon-turtle',
-  Інше: 'icon-other',
+  Собака: "icon-dog",
+  Кіт: "icon-cat",
+  Птах: "icon-bird",
+  Гризун: "icon-rabbit",
+  Плазун: "icon-turtle",
+  Інше: "icon-other",
 };
 
 const petTypePlural: Record<string, string> = {
-  Собака: 'Собаки',
-  Кіт: 'Коти',
-  Птах: 'Птахи',
-  Гризун: 'Гризуни',
-  Плазун: 'Плазуни',
+  Собака: "Собаки",
+  Кіт: "Коти",
+  Птах: "Птахи",
+  Гризун: "Гризуни",
+  Плазун: "Плазуни",
 };
 
 const issueTypeIcons: Record<string, string> = {
-  'Блохи/кліщі': 'icon-insects',
-  'Харчовий розлад': 'icon-eating-disorder',
-  'Шкірні/вушні інфекції': 'icon-skin-infection',
-  'Травмування частин тіла': 'icon-bones',
-  'Догляд та утримання': 'icon-care',
-  'Інше': 'icon-other',
-  'Проблеми з очима': 'icon-eye-problems',
-  'Алергічна реакція': 'icon-allergy',
-  'Проблеми із травленням': 'icon-digestive-problems',
-  
+  "Блохи/кліщі": "icon-insects",
+  "Харчовий розлад": "icon-eating-disorder",
+  "Шкірні/вушні інфекції": "icon-skin-infection",
+  "Травмування частин тіла": "icon-bones",
+  "Догляд та утримання": "icon-care",
+  Інше: "icon-other",
+  "Проблеми з очима": "icon-eye-problems",
+  "Алергічна реакція": "icon-allergy",
+  "Проблеми із травленням": "icon-digestive-problems",
 };
 
 const AboutTab = ({ veterinarian }: Props) => {
   const [showFullText, setShowFullText] = useState(false);
 
   const fullText = veterinarian.description;
-  const shortText = fullText.slice(0, 150) + '...';
+  const shortText = fullText.slice(0, 150) + "...";
 
   return (
     <div className="flex flex-col gap-8 text-gray-900">
@@ -65,37 +64,39 @@ const AboutTab = ({ veterinarian }: Props) => {
           Тварини, з якими працюю:
         </h3>
         <div className="flex flex-row justify-between gap-12">
-           <ul className="columns-2 gap-6 mb-8 space-y-2">
+          <ul className="columns-2 gap-6 mb-8 space-y-2">
             {veterinarian.petTypes?.map((type) => (
-            <li key={type} className="flex items-center gap-2 w-[248px]">
-              <Icon
-                sprite="/sprites/sprite-animals.svg"
-                id={petTypeIcons[type] || 'icon-dog'}
-                width="24px"
-                height="24px"
-                className="stroke-primary stroke-1 scale-x-[-1]"
-              />
-              <p>{petTypePlural[type] || type}</p>
-            </li>
-          ))}
-        </ul>
-      </div>
+              <li key={type} className="flex items-center gap-2 w-[248px]">
+                <Icon
+                  sprite="/sprites/sprite-animals.svg"
+                  id={petTypeIcons[type] || "icon-dog"}
+                  width="24px"
+                  height="24px"
+                  className="stroke-primary stroke-1 scale-x-[-1]"
+                />
+                <p>{petTypePlural[type] || type}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
 
-      <div>
-        <h4 className="text-[18px] font-semibold mb-4">Які проблеми лікую:</h4>
-        <ul className="flex flex-wrap gap-4 mb-8">
-          {veterinarian.issueTypes?.map((issue) => (
-            <li key={issue} className="flex items-center gap-2 w-[248px]">
-              <Icon
-                sprite="/sprites/sprite-problems.svg"
-                id={issueTypeIcons[issue] || 'icon-bones'}
-                width="24px"
-                height="24px"
-                className="stroke-primary stroke-1 scale-x-[-1]"
-              />
-              <p>{issue}</p>
-            </li>
-          ))}
+        <div className="border-b border-gray-100 pb-2">
+          <h4 className="text-[18px] font-semibold mb-4">
+            Які проблеми лікую:
+          </h4>
+          <ul className="flex flex-wrap gap-4 mb-8">
+            {veterinarian.issueTypes?.map((issue) => (
+              <li key={issue} className="flex items-center gap-2 w-[248px]">
+                <Icon
+                  sprite="/sprites/sprite-problems.svg"
+                  id={issueTypeIcons[issue] || "icon-bones"}
+                  width="24px"
+                  height="24px"
+                  className="stroke-primary stroke-1 scale-x-[-1]"
+                />
+                <p>{issue}</p>
+              </li>
+            ))}
             {/* <li className="flex items-center gap-2 w-[248px]">
               <Icon
                 sprite="/sprites/sprite-animals.svg"
