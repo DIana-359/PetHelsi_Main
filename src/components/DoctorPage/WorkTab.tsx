d95406fb17773cd38700ef3ab9f5b079d58cd95a
Anna Velychko <velic@example.com>
2025-09-23 17:44:15 +0200

    fix styles on doctor page

--- src/components/DoctorPage/WorkTab.tsx
+++ src/components/DoctorPage/WorkTab.tsx
@@ -1,24 +1,20 @@
-import { Veterinarian } from '@/utils/types/veterinarian';
-import DocReviews from './DocReviews';
-
-
+import { Veterinarian } from "@/utils/types/veterinarian";
 
 type Props = {
   veterinarian: Veterinarian;
 };
 const WorkTab = ({ veterinarian }: Props) => {
   return (
-    <div className="flex flex-col gap-8">
-      <h3 className="text-[18px] font-semibold mb-4">Поточне місце роботи</h3>
-      <div className="flex flex-col gap-1">
+    <div className="flex flex-col  gap-8">
+      <h3 className=" text-[16px]  md:text-[18px] lg:text-[18px] font-semibold mb-4">
+        Поточне місце роботи
+      </h3>
+      <div className="flex flex-col mb-8 gap-1">
         <p className="text-[16px] text-gray-900">
           {veterinarian.organization.city}
         </p>
-        <p className="text-gray-500">
-          {veterinarian.organization.name}
-        </p>
+        <p className="text-gray-500">{veterinarian.organization.name}</p>
       </div>
-      <DocReviews reviews={veterinarian.reviews} />
     </div>
   );
 };
