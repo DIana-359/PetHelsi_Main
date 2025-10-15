32addec1c25f347050b95bb9b76d217a26976649
Victoriia Kyselova <134308356+VictoriaKiselova@users.noreply.github.com>
2025-10-15 22:13:24 +0300

    Add message component to chats

--- src/app/(site)/owner/chats/page.tsx
+++ src/app/(site)/owner/chats/page.tsx
@@ -1,12 +1,27 @@
-import ChatsNotFound from "@/components/ChatsNotFound/ChatsNotFound";
-import ChatsSidebar from "@/components/ChatsSidebar/ChatsSidebar";
+import ChatsNotFound from "@/components/Chats/ChatsNotFound";
+import ChatsSidebar from "@/components/Chats/ChatsSidebar";
 import chatsListJson from "@/utils/chats.json";
 import type { ChatsResponse } from "@/app/types/chatsTypes";
+import ChatsHeader from "@/components/Chats/ChatsHeader";
+import ChatsMessage from "@/components/Chats/ChatsMessage";
 
-export default async function Chats() {
+export default async function Chats({
+  searchParams,
+}: {
+  searchParams?: { [key: string]: string | string[] | undefined };
+}) {
   const chatsList = chatsListJson as ChatsResponse;
 
   if (!chatsList?.chats || chatsList.chats.length === 0) {
+    return <ChatsNotFound />;
+  }
+
+  const chatId = searchParams?.chatId;
+  const openChat = chatId
+    ? chatsList.chats.find(chat => String(chat.chat_id) === String(chatId))
+    : chatsList.chats[0];
+
+  if (!openChat) {
     return <ChatsNotFound />;
   }
 
@@ -14,8 +29,12 @@ export default async function Chats() {
     <div
       className="bg-background -my-[16px] 
                 md:-ml-[32px] md:-my-[32px] md:pr-0
-                2xl:-ml-[40px] 2xl:-my-[40px]">
+                2xl:-ml-[40px] 2xl:-my-[40px] flex">
       <ChatsSidebar chatsList={chatsList.chats} />
+      <div className="flex-1 p-[16px] pr-0 md:p-[24px] md:pr-0">
+        <ChatsHeader openChat={openChat} />
+        <ChatsMessage openChat={openChat} />
+      </div>
     </div>
   );
 }
---
+++ src/components/Chats/ChatsMessage.tsx
@@ -0,0 +1,39 @@
+import { Chat } from "@/app/types/chatsTypes";
+import clsx from "clsx";
+
+interface ChatsProps {
+  openChat: Chat;
+}
+
+export default function ChatsMessage({ openChat }: ChatsProps) {
+  const { last_login_date, dialogs } = openChat;
+
+  return (
+    <div className="py-[24px] mb-[8px]">
+      <p className="text-center text-[12px] leading-[1] font-[500] text-gray-500 mb-[8px]">
+        {last_login_date.slice(5)}
+      </p>
+      <ul className="flex flex-col">
+        {dialogs.map(elem => (
+          <li
+            key={elem.id}
+            className={clsx(
+              "mb-[8px] min-w-[343px] max-w-[438px]",
+              elem.sender_type === "VET" ? "self-start" : "self-end"
+            )}>
+            <p
+              className={clsx(
+                "p-[12px] rounded-[8px] font-normal text-[14px] leading-[110%] tracking-[0%] text-gray-900 mb-[8px]",
+                elem.sender_type === "VET" ? "bg-gray-450" : "bg-primary-300"
+              )}>
+              {elem.message}
+            </p>
+            <p className="text-[12px] leading-[1] font-[500] text-gray-700">
+              {elem.datetime.slice(11, 16)}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
