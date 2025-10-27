938ac054cd69829d90a740f357e69e1ce0bae6f2
Victoriia Kyselova <134308356+VictoriaKiselova@users.noreply.github.com>
2025-10-27 23:08:00 +0200

    Add saving messages to draft

--- src/app/(site)/owner/chats/page.tsx
+++ src/app/(site)/owner/chats/page.tsx
@@ -28,12 +28,9 @@ export default async function Chats({
   }
 
   return (
-    <div
-      className="w-full bg-background -my-[16px]
-                md:-ml-[32px] md:-my-[32px] md:pr-0
-                2xl:-ml-[40px] 2xl:-my-[40px] flex">
+    <div className="flex-1 bg-background -my-[16px] md:-ml-[32px] md:-my-[32px] md:pr-0 2xl:-ml-[40px] 2xl:-my-[40px] flex">
       <ChatsSidebar chatsList={chatsList.chats} />
-      <div className="flex flex-col pr-0 h-[calc(100vh-143px)] relative w-full">
+      <div className="flex flex-col pr-0 h-[calc(100vh-143px)] relative flex-1">
         <ChatsHeader openChat={openChat} />
         <ChatsMessage openChat={openChat} />
         <MessageInput openChat={openChat} />
