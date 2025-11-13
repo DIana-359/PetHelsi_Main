1ede138f4c864ecaaa2de558b7e8d0a5e04e9830
Anna Velychko <velic@example.com>
2025-11-14 00:56:49 +0100

    fixed getPets from server

---
+++ src/app/api/ownerProfile/get-pets/route.ts
@@ -0,0 +1,35 @@
+import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
+// import { Pet } from "@/app/types/pet";
+
+export async function GET(req: NextRequest) {
+  try {
+    const token = (await cookies()).get("auth-token");
+    if (!token) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+
+    // const body: Pet = await req.json();
+
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/v1/owners/pets`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token.value}`,
+        },
+        // body: JSON.stringify(body),
+      }
+    );
+
+    const data = await res.json();
+    return NextResponse.json(data, { status: res.status });
+  } catch (err) {
+    console.error("Get pet error:", err);
+    return NextResponse.json(
+      { message: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
