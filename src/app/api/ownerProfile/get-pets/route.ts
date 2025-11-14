c0c29495dafce1376f7a0312b540fe38ef485e89
Anna Velychko <velic@example.com>
2025-11-14 01:23:47 +0100

    fixed get-pets route

--- src/app/api/ownerProfile/get-pets/route.ts
+++ src/app/api/ownerProfile/get-pets/route.ts
@@ -1,6 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
 import { cookies } from "next/headers";
-// import { Pet } from "@/app/types/pet";
 
 export async function GET(req: NextRequest) {
   try {
@@ -8,8 +7,6 @@ export async function GET(req: NextRequest) {
     if (!token) {
       return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
     }
-
-    // const body: Pet = await req.json();
 
     const res = await fetch(
       `${process.env.NEXT_PUBLIC_BASE_URL}/v1/owners/pets`,
@@ -19,7 +16,6 @@ export async function GET(req: NextRequest) {
           "Content-Type": "application/json",
           Authorization: `Bearer ${token.value}`,
         },
-        // body: JSON.stringify(body),
       }
     );
 
