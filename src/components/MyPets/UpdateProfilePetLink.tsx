f0f53e189966c9d6056acc224661f02952435b02
Anna Velychko <velic@example.com>
2026-01-25 23:10:20 +0100

    change editPage link

--- src/components/MyPets/UpdateProfilePetLink.tsx
+++ src/components/MyPets/UpdateProfilePetLink.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Icon from "../Icon";
 
 export default function UpdateProfilePetLink() {
+  const petId = "123";
   return (
     <div className="py-[10px] px-[16px] flex items-center gap-[8px] group">
       <Icon
@@ -11,7 +12,7 @@ export default function UpdateProfilePetLink() {
         height="20px"
         className="stroke-gray-700 fill-background group-hover:stroke-primary-900"
       />
-      <Link href={"/owner/pets/edit-pet"}>
+      <Link href={`/owner/pets/edit-pet/${petId}`}>
         <span className="text-[14px] font-[400] leading-[1] text-primary-700 group-hover:text-primary-900">
           Редагувати
         </span>
